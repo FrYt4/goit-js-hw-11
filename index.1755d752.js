84186b5835678b9d9dfe013fd176487d1206df57
FrYt4 <145793325+FrYt4@users.noreply.github.com>
2024-02-24 12:50:59 +0000

    Deploying to gh-pages from @ FrYt4/goit-js-hw-11@b9f714bcc29683a2bbe7d7866d210962598f2c01 🚀

---
+++ index.1755d752.js
@@ -0,0 +1,2 @@
+fetch("https://pixabay.com/api/?key=42538770-38c8e4bc557ccec23452e1973&q=yellow+flowers&image_type=photo");
+//# sourceMappingURL=index.1755d752.js.map
---
+++ index.a516e926.js
@@ -0,0 +1,2 @@
+fetch("https://pixabay.com/api/?key=42538770-38c8e4bc557ccec23452e1973&q=yellow+flowers&image_type=photo");
+//# sourceMappingURL=index.a516e926.js.map
